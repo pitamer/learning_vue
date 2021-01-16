2f3d737a64846bd4dd30998fa8e593193500698d
pitamer <itamar@codicate.com>
2021-01-16 13:41:44 +0200

    Use the v-if directive

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 app = new Vue ({
     el: '#app',
     data: {
-        my_message: 'Hello Vue, a true open-source framework thingy, not backed by EvilCorp LTD :)'
+        my_message: 'A message that I have written',
+        span_1_visible: true,
+        span_2_visible: false
     }
 })
+
+setInterval(
+    () => {
+        app.span_1_visible = !app.span_1_visible;
+        app.span_2_visible = !app.span_2_visible
+    },
+    1000
+)
