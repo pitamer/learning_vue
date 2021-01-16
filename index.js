d573da715d4fcc0c7c085ac4d22c7c36d0bfee4b
pitamer <itamar@codicate.com>
2021-01-16 13:54:30 +0200

    Use v-for alongside v-if

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-app = new Vue ({
-    el: '#app',
-    data: {
-        my_message: 'A message that I have written',
-        span_1_visible: true,
-        span_2_visible: false
-    }
-})
+app = new Vue({
+  el: "#app",
+  data: {
+    my_message: "A message that I have written",
+    spans: [
+      {
+        name: "span 1",
+        visible: true,
+        color: "blue",
+      },
+      {
+        name: "span 2",
+        visible: true,
+        color: "purple",
+      },
+      {
+        name: "span 3",
+        visible: true,
+        color: "red",
+      },
+      {
+        name: "span 4",
+        visible: true,
+        color: "yellow",
+      },
+      {
+        name: "span 5",
+        visible: true,
+        color: "green",
+      },
+    ],
+  },
+});
 
 setInterval(
     () => {
-        app.span_1_visible = !app.span_1_visible;
-        app.span_2_visible = !app.span_2_visible
+        app.spans.forEach(span => {
+            setTimeout(() => {
+                span.visible = false
+            }, 1000 + 200*app.spans.indexOf(span))
+            span.visible = true
+        })
     },
-    1000
+    2000
 )
