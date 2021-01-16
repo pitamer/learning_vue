e3d1b9c2950a0f5a224537f8a58223f870db3bec
pitamer <itamar@codicate.com>
2021-01-16 12:41:50 +0200

    And now it works :)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 app = new Vue ({
     el: '#app',
     data: {
-        message: 'Hello Vue, a true open-source framework thingy, not backed by EvilCorp LTD :)'
+        my_message: 'Hello Vue, a true open-source framework thingy, not backed by EvilCorp LTD :)'
     }
 })
