84d9c4fe74e52ece927ac90a9c3409ff26080b83
pitamer <itamar@codicate.com>
2021-01-20 23:31:30 +0200

    Everything is a component now muhahahaha

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+Vue.component("app-title", {
+  data: function() {
+    return {
+      title: "A TODOs app",
+      subTitle: "'cause I'm feeling creative today",
+    }
+  },
+  template: `<div class="title">
+              <h1>{{ title }}</h1>
+              <h5>{{ subTitle }}</h5>
+            </div>`
+});
+
 Vue.component("todo-item", {
   props: ["todo"],
   data: () => {
@@ -15,11 +28,46 @@ Vue.component("todo-item", {
             </li>`,
 });
 
+Vue.component("new-todo-form", {
+  props: ["todo"],
+  data: () => {
+    return {
+      newTodoInput: "",
+      addTodo: (newTodoInput) => {
+        todo = {
+          text: newTodoInput,
+          done: false,
+        };
+        if (todo.text) {
+          app.todos.push(todo);
+          app.newTodoInput = "";
+        } else {
+          console.log("Nay.");
+        }
+      },
+    };
+  },
+  template: `<div class="new-todo-container">
+            <form v-on:submit.prevent class="new-todo-form">
+                <input
+                    type="text"
+                    class="new-todo-input"
+                    v-model="newTodoInput"
+                >
+                <button
+                    type="submit"
+                    class="new-todo-button"
+                    v-on:click="addTodo(newTodoInput)"
+                >
+                    Add!
+                </button>
+            </form>
+        </div>`,
+});
+
 app = new Vue({
   el: "#app",
   data: {
-    title: "A TODOs app",
-    subTitle: "'cause I'm feeling creative today",
     todos: [
       {
         text: "Take hugsy for a walk",
@@ -36,22 +84,7 @@ app = new Vue({
       {
         text: "Meditate for 15 minutes",
         done: false,
-      },
-    ],
-    newTodoInput: "",
-  },
-  methods: {
-    addTodo: (newTodoInput) => {
-      todo = {
-        text: newTodoInput,
-        done: false,
-      };
-      if (todo.text) {
-        app.todos.push(todo);
-        app.newTodoInput = "";
-      } else {
-        console.log("Nay.");
       }
-    },
-  },
+    ]
+  }
 });
